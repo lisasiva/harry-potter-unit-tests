cfccfdc65a04f345fc166d6e8e9e03974ab5cc23
lisasiva <lisashantisiva@gmail.com>
2019-11-20 18:06:12 -0800

    Added tests for keys() and values()

--- hashtable.js
+++ hashtable.js
@@ -32,14 +32,32 @@ table.set('quietus', 'diminishes voice');
 table.set('stupefy', 'stuns opponent');
 table.set('morsmorde', 'conjures dark mark');
 
-console.log('\nget(key)')
-assertEqual('lights tip of wand', table.get('lumos'), 'returns correct pair from index with no collisions');
-assertEqual('conjures dark mark', table.get('morsmorde'), 'returns correct pair from index with collisions');
-assertEqual(undefined, table.get('tarantallegra'), 'returns undefined if key is not found');
-//assertEqual('slows an advancing opponent', table.get('iMpedimEnta'), 'works with differently cased key');
-
-
 // Collisions:
 // 29 - crucio, morsmorde
 // 33 - sectumsempra, avis
 // 39 - avada kedavra, stupefy
+
+console.log('\nget(key)')
+assertEqual('lights tip of wand', table.get('lumos'), 'returns correct pair from index with no collisions');
+assertEqual('conjures dark mark', table.get('morsmorde'), 'returns correct pair from index with collisions');
+assertEqual(undefined, table.get('tarantallegra'), 'returns undefined if key is not found');
+
+// Optional
+//assertEqual('slows an advancing opponent', table.get('iMpedimEnta'), 'works with differently cased key');
+
+console.log('\nkeys()');
+const keys = table.keys();
+assertEqual(28, keys.length, 'returns correct number of keys with no extra spaces');
+assertEqual(true, keys.indexOf('expecto patronum') !== -1, 'returns key at index with only one pair');
+assertEqual(true, keys.indexOf('crucio') !== -1, 'returns all keys at index with multiple pairs');
+assertEqual(true, keys.indexOf('morsmorde') !== -1, 'returns all keys at index with multiple pairs');
+
+console.log('\nvalues()');
+const values = table.values();
+assertEqual(true, values.indexOf('levitates an object') !== -1, 'returns value at index with only one pair');
+assertEqual(true, values.indexOf('unforgivable') !== -1, 'returns all values at index with multiple pairs');
+assertEqual(true, values.indexOf('conjures dark mark') !== -1, 'returns all values at index with multiple pairs');
+assertEqual(26, values.length, 'returns correct number of keys with no extra spaces, no repeats');
+
+// If returning all values, including repeats
+//assertEqual(28, values.length, 'returns correct number of keys with no extra spaces');
